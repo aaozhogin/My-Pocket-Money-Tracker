479f0906a1d318e79e74ea82beda297133cd556f
Aleksandr Ozhogin <aaozhogin@gmail.com>
2019-09-09 19:35:31 +1000

    Initial Commit

---
+++ My Pocket Money Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Pocket Money Tracker
+//
+//  Created by Aleksandr Ozhogin on 9/9/19.
+//  Copyright © 2019 Aleksandr Ozhogin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
